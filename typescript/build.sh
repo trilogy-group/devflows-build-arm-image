447e6a9fe4c06d633b89d93404e78577065afd9d
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-20 22:51:02 +0530

    fixed build script for typescript

--- typescript/build.sh
+++ typescript/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-aws s3 cp s3://devflows-function-framework/typescript/setup.sh ./.devflows/setup.sh
-chmod +x ./.devflows/setup.sh
-./.devflows/setup.sh
-docker build -f .devflows/Dockerfile -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
+cp ../${RUNTIME_GITOPS_REPOSITORY}/typescript/Dockerfile.arm64 ./Dockerfile
+docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
 docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
