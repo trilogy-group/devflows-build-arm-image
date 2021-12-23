d7fef6840c4c74f4a4778cf62e211567a249dcb1
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-23 16:31:02 +0530

    added support for java

--- java/build.sh
+++ java/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Skipping for Java. Not yet supported."
+if test -f "devhub/Dockerfile"; then
+    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f devhub/Dockerfile .
+    docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+else        
+    cp ../${RUNTIME_GITOPS_REPOSITORY}/java/Dockerfile.arm64 ./Dockerfile
+    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
+    docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+fi
