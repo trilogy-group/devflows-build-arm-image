a80dddd825fdff2d5b57a1bffff022b47fbc729b
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-24 03:59:14 +0530

    handles gradlew build for java

--- java/build.sh
+++ java/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if test -f "devhub/Dockerfile"; then
+if test -f "gradlew"; then
+    yum update && amazon-linux-extras install java-openjdk11 && yum install java-11-openjdk-devel -y
+    ./gradlew clean assemble
+    cp ../logs-extension-arm64 ./lambda/build/extensions/logs-extension
+    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f lambda/src/main/docker/Dockerfile.lambda.arm64 lambda
+    docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+elif test -f "devhub/Dockerfile"; then
     docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f devhub/Dockerfile .
     docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 else        
