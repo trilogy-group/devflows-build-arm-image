4da017f4fd9f049cb0f9c07d5171654009defb22
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-23 22:23:49 +0530

    fixed for python

--- python/build.sh
+++ python/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ../${RUNTIME_GITOPS_REPOSITORY}/typescript/Dockerfile.arm64 ./Dockerfile
+cp ../${RUNTIME_GITOPS_REPOSITORY}/python/Dockerfile.arm64 ./Dockerfile
 docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
 docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
