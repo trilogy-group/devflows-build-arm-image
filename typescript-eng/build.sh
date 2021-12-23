3c4585dac6a138c131e0ff5c9f7904b81ca45826
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-23 16:31:20 +0530

    added support for typescript-eng

--- typescript-eng/build.sh
+++ typescript-eng/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-aws s3 cp s3://devflows-function-framework/typescript-eng/setup.sh ./.devflows/setup.sh
-chmod +x ./.devflows/setup.sh
-./.devflows/setup.sh
-
+cp ../${RUNTIME_GITOPS_REPOSITORY}/typescript-eng/Dockerfile.arm64 ./Dockerfile
+# Install dependencies
+npm run ci-all || npm ci
+# Build
 npm run build
 
 echo "DF_FUNCTIONS_VERSION=${IMAGE_TAG}" >> ./.devflows/.env
-
-aws s3 cp s3://devflows-function-framework/typescript-eng/setup.sh ./.devflows/setup.sh
-chmod +x ./.devflows/setup.sh
-./.devflows/setup.sh
-
-docker build -f .devflows/Dockerfile -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
+docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} .
 docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
