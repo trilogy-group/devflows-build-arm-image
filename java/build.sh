7db7d3565b434e2780b9a27bbae2edec8fdb7673
cn-devflows-prod <devflows-operations@devfactory.com>
2021-12-27 15:18:47 +0530

    copy logs-extension after gradle build

--- java/build.sh
+++ java/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if test -f "gradlew"; then
-    cp ../logs-extension-arm64 ./lambda/build/extensions/logs-extension
     yum update && amazon-linux-extras install java-openjdk11 && yum install java-11-openjdk-devel -y
     ./gradlew clean assemble
+    cp ../logs-extension-arm64 ./lambda/build/extensions/logs-extension
     docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f lambda/src/main/docker/Dockerfile.lambda.arm64 lambda
     docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 elif test -f "devhub/Dockerfile"; then
