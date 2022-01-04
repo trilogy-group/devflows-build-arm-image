80a2484371760b646179d5d9e44585b25072d257
cn-devflows-prod <devflows-operations@devfactory.com>
2022-01-05 01:25:39 +0530

    added separate dockerfiles for django and fastapi

--- python/build.sh
+++ python/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if test -f 'devhub/Dockerfile'; then
-    echo "Framework is either Django or FastAPI"
-    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f devhub/Dockerfile .
+if test -f 'manage.py'; then
+    echo "Django Framework"
+    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f ../Dockerfile.django .
+    docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+elif test -f "main.py"; then 
+    echo "FastAPI Framework"
+    docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} -f ../Dockerfile.fastapi .
     docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 else
     cp ../${RUNTIME_GITOPS_REPOSITORY}/python/Dockerfile.arm64 ./Dockerfile
